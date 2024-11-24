ebee9a9332acaa4f87065642c1b8aef88c1df2f8
RocioRosalindaLoboJacinto <lobojacintorociorosalinda#@gmail.com>
2024-11-23 19:46:56 -0500

    mi primer app en fluter

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:mi_primera_app/my_home_page.dart';
-import 'package:mi_primera_app/providers/app_state.dart';
+import 'package:mi_primera_app/screen/my_home_page.dart';
+import 'package:mi_primera_app/providers/my_app_state.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const Myapp());
+  runApp(MyApp());
 }
 
-class Myapp extends StatelessWidget {
-  const Myapp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => AppState(),
+      create: (context) => MyAppState(),
       child: MaterialApp(
-        title: 'mi primera app',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        ),
+        title: 'My primera app',
+        debugShowCheckedModeBanner: false,
         home: MyHomePage(),
       ),
     );
